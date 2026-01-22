6c611dc900b029cfdbe937627ee09a289d349e8b
Eduardo Jimenez <eddy.jimenz@yahoo.com>
2026-01-22 15:34:57 -0600

    Add files via upload

---
+++ skymap/js/main.js
@@ -0,0 +1,7 @@
+import { state, subscribe } from "./store.js";
+import "./render/skymapRenderer.js";
+import "./export/export.js";
+
+// Aquí solo vive la orquestación.
+// Tu lógica original sigue intacta en los otros módulos.
+console.log("SkyMap refactor listo");
